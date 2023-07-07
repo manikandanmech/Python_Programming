42a269e79464e22b634a708f00d76de90ae2a673
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-07-07 21:20:00 +0530

    Add files via upload

---
+++ Range in Python.py
@@ -0,0 +1,16 @@
+#Range in Python
+"""
+The range() function creates an iterator that generates a sequence of numbers within a given range.
+"""
+# Range in Python
+"""
+1-5  =>0,1,2,3,4
+0-5 =>2,4  +2
+range(5)  =>0,1,2,3,4
+range(2,5)  =>
+"""
+
+print(list(range(5)))
+print(list(range(2, 5)))  # n-1
+print(list(range(0, 31, 2)))
+print(list(range(1, 30, 2)))
