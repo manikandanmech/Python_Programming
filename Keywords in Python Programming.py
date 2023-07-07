645276b28a953ad7d83d1255cdae5e66882476cc
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-07-07 15:55:02 +0530

    Add files via upload

---
+++ Keywords in Python Programming.py
@@ -0,0 +1,24 @@
+# Keywords in Python Programming
+"""
+       Keywords are the words that convey a special meaning to the language compiler/interpreter.
+These are reserved for special purpose and must not be used as normal identifier names.
+
+ Eg: for,break,continue,etc.
+
+A keyword is a word having special meaning reserved by programming language
+
+We cannot use a keyword as a variable name, function name or any other identifier.
+
+True and False are truth values in Python. They are the results of comparison operations or logical (Boolean) operations in Python.
+
+None is a special constant in Python that represents the absence of a value or a null value.
+
+and, or, not are the logical operators in Python. and will result into True only if both the operands are True.
+
+
+The async and await keywords are provided by the asyncio library in Python. They are used to write concurrent code in Python.
+"""
+
+# Source Code
+import keyword
+print(keyword.kwlist)
