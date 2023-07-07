d2a7188c8863f99cd0148d6bf6a35376a10d2bdd
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-07-07 18:28:50 +0530

    Add files via upload

---
+++ Comparison Operators or Relational Operators in Python.py
@@ -0,0 +1,22 @@
+#Comparison Operators or Relational Operators in Python
+
+""" Relational operators compares the values of two operands and returns TRUE or FALSE based on whether the condition is met.
+-----------------------------------------------------
+Operator        	uses
+-----------------------------------------------------
+==    	      Equal operator
+!=      	Not Equal operator
+<	        Less than operator
+>	        Greater than operator
+<=	        Less than or equal to operator
+>=	        Greater than or equal to operator
+----------------------------------------------------
+ """
+a = 10
+b = 20
+print(a == b)
+print(a != b)
+print(a > b)
+print(a < b)
+print(a >= b)
+print(a <= b)
