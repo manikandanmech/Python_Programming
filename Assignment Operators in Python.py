de0457e2218cc8eabf7962d2576d7c28e934accb
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-07-07 18:22:33 +0530

    Add files via upload

---
+++ Assignment Operators in Python.py
@@ -0,0 +1,48 @@
+#Assignment Operators in Python
+"""  Assignment operators are used to assigning value to a variable.
+The left side operand of the assignment operator is a variable and right side operand of the assignment operator is a value.
+This operator is used to assign the value on the right to the variable on the left
+-----------------------------------------------------------
+Compound Operator	Sample Expression	Expanded Form
+----------------------------------------------------------
+ +=	                   a+=2             	a=a+2
+ -=	                   a-=6             	a=a-6
+ *=	                   a*=7	                a=a*7
+ /=	                   a/=4              	a=a/4
+ %=                    a%=9	                a=a%9
+ **=                   a**=3              	a=a**3
+ //=               	   a//=2	            a=a//2
+----------------------------------------------------------  """
+# Assignment Operators
+
+"""
+=   Assignment
++=	Addition
+-=	Subtraction
+*=	Multiplication
+/=	Division
+%=	Modulus
+**=	Exponentiation
+//=	Floor division
+"""
+
+a = 100
+print(a)
+a += 5  # a=a+5
+print(a)
+a -= 10  # a=a-10
+print(a)
+a *= 10  # a=a*10
+print(a)
+a /= 10
+print(a)
+a %= 10
+print(a)
+a **=10
+print(a)
+a //= 10
+print(a)
+
+
+
+
