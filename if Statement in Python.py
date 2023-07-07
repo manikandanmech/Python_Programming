95bee541e6e0ae06b7a8cd03d58e11c59ed42753
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-07-07 18:43:58 +0530

    Add files via upload

---
+++ if Statement in Python.py
@@ -0,0 +1,15 @@
+#if Statement in Python
+"""
+ The if statement is the most basic of all the control flow statements.
+ It tells your program to execute a certain section of code only if a particular test evaluates to true.
+ The if statement is written with the if keyword.
+
+  Syntax :>>
+        if ( condition ) :
+               // body of the statements will execute if the condition is true
+"""
+
+# if Statement in Python
+n = int(input("Enter The Number : "))
+if n % 2 == 0:
+    print(n, " is Even Number")
