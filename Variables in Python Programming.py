ae989b2ca043dc03ff863f4fcb899474871534a1
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-07-07 16:05:05 +0530

    Add files via upload

---
+++ Variables in Python Programming.py
@@ -0,0 +1,30 @@
+#Variables in Python Programming
+"""
+In programming, a "variable" is a container in which a data value can be stored within the computer’s memory.
+"""
+#Syntax:
+variable_name = variable_value
+
+#Example:
+a = 10;
+name = "Joes"
+
+
+""" 
+>>>> rules for creating and using variables in Python:
+
+* Variable names can only contain letters, numbers, and underscores. They cannot start with a number.
+* Variable names should be descriptive and meaningful, and should not contain spaces. 
+* It's common to separate words in a variable name with an underscore.
+* Variable names should not conflict with Python keywords, such as print, if, or for.
+* Python is case sensitive, so name and Name are considered two different variables.
+* A variable must be assigned a value before it can be used.   
+
+"""
+
+#Example for Valid Variables
+
+name= "Ram"
+User_name= "Ram"
+name2= "Ram"
+
