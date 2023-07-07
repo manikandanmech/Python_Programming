7285c67ce5955994a9f0b19abe5ca332991bfa40
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-07-07 16:59:16 +0530

    Add files via upload

---
+++ Comment in Python.py
@@ -0,0 +1,29 @@
+#                                          Single and Multiline Comment in Python
+"""
+In Python, there are two types of comments:
+
+single-line
+multi-line
+
+Single-line comments start with a hash symbol (#) and extend to the end of the line:
+Multi-line comments start and end with three quotation marks(''' or three time(").
+
+"""
+
+#>>> Source Code
+# Basic Program in Python
+# Basic Program in Python
+# Basic Program in Python
+
+'''
+Basic Program in Python
+Basic Program in Python
+Basic Program in Python
+Basic Program in Python
+Basic Program in Python
+'''
+
+a = 10
+b = 20
+c = a + b
+print(c)
