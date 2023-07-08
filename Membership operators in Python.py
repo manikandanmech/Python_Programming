123488699807d2f3493b256459be4c11584e961a
MANIKANDAN.C <112545424+manikandanmech@users.noreply.github.com>
2023-07-08 22:14:18 +0530

    Add files via upload

---
+++ Membership operators in Python.py
@@ -0,0 +1,10 @@
+                   #Membership operators in Python
+"""
+Membership operators are used to test if a sequence is presented in an object.
+The use membership operators to check whether a value or variable exists in a sequence (string, list, tuples, sets, dictionary) or not.
+They are two membership python operators in and not in.
+
+"""
+a=[10,25,45,88]
+print(25 in a)
+print(25 not in a)
